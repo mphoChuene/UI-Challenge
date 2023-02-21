99bd4524bafeb91bdfdef901f90f50e11b1ccbd5
mphoChuene <mphochuene42@gmail.com>
2023-02-21 21:43:20 +0200

    project and skill-set update in javaScript

--- bootcamp2023-main/aboutus.js
+++ bootcamp2023-main/aboutus.js
@@ -1,3 +1,5 @@
+            /* programming-language skills arrays  */
+
 let skills = new Array (
     {img:"assets/logo/HTML5_logo.svg", skill:"HTML", level: "9/10"},
     {img:"assets/logo/css-3-logo-png-transparent.png", skill:"CSS", level: "7/10"},
@@ -10,8 +12,11 @@ let skills = new Array (
   let listUl = "<div style='width:500px'>";
     for (let i = 0; i < skills.length; i++) {
       console.log(skills[i].item);
+
+
+            /* table structure concatination and element-incrementation */
       listUl +=
-        "<table style='width: 100%; margin: 8px; border:1px solid grey; border-radius: 10px;'><tr><td rowspan='3' style='width:150px'><img src='" +
+        "<table style='width: 100%; margin: 8px; border:1px solid grey; border-radius: 50px;'><tr><td rowspan='3' style='width:150px'><img src='" +
         skills[i].img +
         "' alt='' style='width: 100px; height: 100px; object-fit: cover; border-radius: 50%;' />" +
         "</td></tr> <tr><td>Skill: " +
@@ -22,3 +27,46 @@ let skills = new Array (
     }
     listUl += "</div>";
     document.getElementById("skillsList").innerHTML = listUl;
+
+            
+    /*      project section in javaScript */
+
+function elementFromHtml(html) {
+  const template = document.createElement("template");
+  template.innerHTML = html.trim();
+
+  return template.content.firstElementChild
+}
+/*  container to store all the  html elements in javaScript using a
+ multi-line string */
+
+const myproject = elementFromHtml(`
+  <div>
+  <table>
+    <tr>
+      <td style="padding-left: 12vh;">
+        <div class="card" style="width: 18rem; margin-left: 12vh;" >
+          <img src="assets/imgs/portfolio1.PNG" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">personal Portfolio</h5>
+            <p class="card-text">This is a Portfolio website, i created in my second year of studying to showcase my skills.The <strong> Tech stack </strong> used include: HTML5, CSS3, JavaScript</p>
+            <a href="https://wilportfolio19340485.000webhostapp.com/" class="btn btn-primary">Visit Page</a>
+          </div>
+        </div>
+      </td>
+      <td style="padding-left: 25vh;">
+        <div class="card" style="width: 18rem; margin-left: 12vh;">
+          <img src="assets/imgs/mural.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Graphic Design</h5>
+            <p class="card-text">This is a mural, i created to showcase my Design and Creativity skills, the <strong> application </strong> used include: Adobe photoshop and Adobe Illustrator </p>
+            <a href="assets/imgs/mural.png" class="btn btn-primary">View Image</a>
+          </div>
+        </div>
+      </td>
+    </tr>
+  </table>
+  </div>
+  `)
+
+document.getElementById('my-proj').appendChild(myproject)
